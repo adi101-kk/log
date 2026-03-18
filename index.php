6712eeb782075ec8f755b628200d35d9c207a6cf
adi101-kk <adityakumar896911@gmail.com>
2026-03-18 20:38:51 +0530

    final1

--- index.php
+++ index.php
@@ -9,21 +9,21 @@
 <body>
     <div class="container" id="signup" style="display: none;">
         <h1 class="form-title">CREATE ACCOUNT</h1>
-        <form method="post" action="">
+        <form method="post" action="register.php">
             <div class="input-group">
-                <input type="text" placeholder="First name" required>
+                <input type="text" name="fName" placeholder="First name" required>
             </div>
             <div class="input-group">
-                <input type="text" placeholder="Last name" required>
+                <input type="text" name="lName" placeholder="Last name" required>
             </div>
             <div class="input-group">
-                <input type="email" placeholder="Email" required>
+                <input type="email" name="email" placeholder="Email" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Password" required>
+                <input type="password" name="password" placeholder="Password" required>
             </div>
             <div class="end">
-                <button type="submit" >Create</button>
+                <button type="submit" name="signup">Create</button>
                 <button type="button" id="signInButton">Sign in</button>
             </div>
         </form>
@@ -31,18 +31,18 @@
     <!-- Sign in -->
      <div class="container" id="signin">
         <h1 class="form-title">SIGN IN</h1>
-        <form method="post" action="">
+        <form method="post" action="register.php">
             <div class="input-group">
-                <input type="email" placeholder="Email" required>
+                <input type="email" name="email" placeholder="Email" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Password" required>
+                <input type="password" name="password" placeholder="Password" required>
             </div>
             <p class="recover">
                 <a href="#">Forgot password?</a>
             </p>
             <div class="end">
-                <button type="submit">Sign in</button>
+                <button type="submit" name="signin">Sign in</button>
                 <button type="button" id="signUpButton">Create</button>
             </div>
         </form>
